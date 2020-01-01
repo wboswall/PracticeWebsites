0dbc2ac74f423744a56f269ebb4611e4aa5eeb9a
wboswall <wboswall@live.com>
2020-01-01 18:08:20 -0400

    Commit HelloworldWebSite
    Moved from testprograms

---
+++ HelloWorldWebSite/engine.js
@@ -0,0 +1,10 @@
+ /*
+    Folder:	HelloWorldWebSite
+    File:	engine.js
+    Associated files: index.php, style.css, engine.js, helloWebSiteLib.php,and names.mysql
+    Author:	@wboswall
+    Copyright:	(C)2017 All Rights Reserved.
+    Description:	This will drive the website on the frontend. 
+    Date Created:	2017-12-03
+    Date Modified:	2017-12-18
+  */
---
+++ HelloWorldWebSite/helloWebSiteLib.php
@@ -0,0 +1,13 @@
+<?
+  /*
+    Folder:	HelloWorldWebPage
+    File:	index.html
+    Associated files: index.php, style.css, engine.js, and names.mysql
+    Author:	@wboswall
+    Copyright:	(C)2017 All Rights Reserved.
+    Description:	This file will do all of the processing at the backend.
+    Date Created:	2017-12-03
+    Date Modified:	2017-12-18
+  */
+  
+?>
