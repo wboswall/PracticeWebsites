25c7cbae02ca2a0b1eaad7c67097d4e82baac1f9
wboswall <wboswall@live.com>
2020-01-01 18:07:43 -0400

    Commit helloWorldWebPage
    Moved from testprograms.

---
+++ helloWorldWebPage/engine.js
@@ -0,0 +1,9 @@
+/*
+	Folder:	HelloWorldWebPage
+	File:	engine.js
+	Author:	@wboswall
+	Copyright:	(C)2017 All Rights Reserved.
+	Description:	Simply controls the frontend logic.
+	Date Created:	2017-12-03
+	Date Modified:	2017-12-03
+*/
